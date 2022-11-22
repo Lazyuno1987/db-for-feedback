ffcbc0b84c9675236d2fd0260fb7f613daed9c87
Lazyuno1987 <95475727+Lazyuno1987@users.noreply.github.com>
2022-11-22 23:43:55 +0200

    df

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("dotenv").config()
 const {DB_HOST, PORT=3000}=process.env
 
 mongoose
-  .connect(DB_HOST, {useNewUrlParser:true,  useUnifiedTopology: true})
+  .connect(DB_HOST, {useNewUrlParser:true,  useUnifiedTopology: true, useCreateIndex: true})
   .then(() => {
     console.log(`Database connection successful ${PORT}`);
     app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`))
